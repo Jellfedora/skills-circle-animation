d57e390495cabad20839ea7977343751ab2c1683
Jellfedora <julienlecointe@live.fr>
2020-09-15 21:45:00 +0200

    Added window size setup

--- index.js
+++ index.js
@@ -24,18 +24,14 @@ if (window.innerWidth > 800) {
 
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-};
-
 // -----------------------------------------------------------------
 // CAMERA
-
 var camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 30);
+if (window.innerWidth < 1024) {
+    camera.position.set(0, 0, 50);
+} else {
+    camera.position.set(0, 0, 35);
+}
 
 
 // -----------------------------------------------------------------
@@ -48,8 +44,10 @@ scene.background = new THREE.Color("black");
 // TOOLS
 
 // OrbitControls
-// const controls = new OrbitControls(camera, canvas);
-// controls.update();
+const controls = new OrbitControls(camera, canvas);
+controls.update();
+controls.enableDamping = true;
+controls.dampingFactor = 0.08;
 
 // -----------------------------------------------------------------
 // LIGHT
@@ -58,122 +56,95 @@ var light = new THREE.AmbientLight('white'); // soft white light
 scene.add(light);
 
 //----------------------------------------------------------------- 
-// FUNCTION
+// FUNCTIONS
 
-// 0,272727273
 var skillsArray = [
     {
-        name: 'Html5',
-        position: [0, 6, 0]
+        name: 'Html5'
     },
     {
-        name: 'Css3',
-        position: [0, 5, 0]
+        name: 'Css3'
     },
     {
-        name: 'Sass',
-        position: [0, 4, 0]
+        name: 'Sass'
     },
     {
-        name: 'Bootstrap',
-        position: [0, 3, 0]
+        name: 'Bootstrap'
     },
     {
-        name: 'Javascript',
-        position: [0, 2, 0]
+        name: 'Javascript'
     },
     {
-        name: 'ReactJs',
-        position: [1, 6, 0]
+        name: 'ReactJs'
     },
     {
-        name: 'Redux',
-        position: [1, 5, 1]
+        name: 'Redux'
     },
     {
-        name: 'NodeJs',
-        position: [1, 4, 1]
+        name: 'NodeJs'
     },
     {
-        name: 'Symfony',
-        position: [1, 3, 1]
+        name: 'Symfony'
     },
     {
-        name: 'AngularJs',
-        position: [1, 2, 1]
+        name: 'AngularJs'
     },
     {
-        name: 'Cakephp',
-        position: [0, 1, 1]
+        name: 'Cakephp'
     },
     {
-        name: 'Codeigniter',
-        position: [0, 1, 1]
+        name: 'Codeigniter'
     },
     {
-        name: 'Three.js',
-        position: [0, 1, 1]
+        name: 'Three.js'
     },
     {
-        name: 'mysql',
-        position: [0, 1, 1]
+        name: 'mysql'
     },
     {
-        name: 'php',
-        position: [0, 1, 1]
+        name: 'php'
     },
     {
-        name: 'Wordpress',
-        position: [0, 1, 1]
+        name: 'Wordpress'
     },
     {
-        name: 'Git',
-        position: [0, 1, 1]
+        name: 'Git'
     },
     {
-        name: 'Gitlab',
-        position: [0, 1, 1]
+        name: 'Gitlab'
     },
     {
-        name: 'Github',
-        position: [0, 1, 1]
+        name: 'Github'
     },
     {
-        name: 'Linux',
-        position: [0, 1, 1]
+        name: 'Linux'
     },
     {
-        name: 'Windows',
-        position: [0, 1, 1]
+        name: 'Windows'
     },
     {
-        name: 'React Native',
-        position: [0, 1, 1]
+        name: 'React Native'
     },
     {
-        name: 'Electron',
-        position: [0, 1, 1]
+        name: 'Electron'
     },
     {
-        name: 'Agile',
-        position: [0, 1, 1]
+        name: 'Agile'
     },
     {
-        name: 'Merise',
-        position: [0, 1, 1]
+        name: 'Merise'
     },
 ];
 
 
 //----------------------------------------------------------------- 
 // Sphere
-
 var radius = 5;
 var widthSegments = 6;
 var heightSegments = 5;
 var geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 
-// Sphere Transparent color
+// Transparent color
 var material = new THREE.MeshPhongMaterial({
     color: 'white',
     opacity: 0,
@@ -184,9 +155,7 @@ var sphereVerticesArray = geometry.vertices;
 var sphereIsAnimate = true;
 
 // Plane
-
 var planeGeometry = new THREE.PlaneGeometry(7, 4, 4, 4);
-// var planeMaterial = new THREE.MeshBasicMaterial({ color: 'red', side: THREE.DoubleSide });
 var plane = new THREE.Mesh(planeGeometry, material);
 var planeVerticesArray = plane.geometry.vertices;
 scene.add(plane);
@@ -194,8 +163,6 @@ scene.add(plane);
 // Create Text Elements
 function createTextElements() {
     const colorSkill = new THREE.MeshPhongMaterial({ color: 'white' });
-    console.log(plane.geometry)
-    console.log(sphere.geometry)
     // Pour chaque élément du tableau on crée un mesh
     for (let index = 0; index < skillsArray.length; index++) {
 
@@ -218,31 +185,6 @@ function createTextElements() {
     }
 }
 
-// function createTextElements() {
-//     const colorSkill = new THREE.MeshPhongMaterial({ color: 'white' });
-//     console.log(plane.geometry)
-//     console.log(sphere.geometry)
-//     // Pour chaque élément du tableau on crée un mesh
-//     for (let index = 0; index < skillsArray.length; index++) {
-
-//         let skillName = skillsArray[index].name;
-//         var skill = new THREE.FontLoader();
-
-//         skill.load('Caveat_Regular.json', function (font) {
-
-//             let geometry = new THREE.TextGeometry(skillName, {
-//                 font: font,
-//                 size: 0.2,
-//                 height: 0.1,
-//                 curveSegments: 0.01,
-//             });
-//             let skillMesh = new THREE.Mesh(geometry, colorSkill);
-
-//             skillMesh.position.set(plane.geometry.vertices[index].x, plane.geometry.vertices[index].y, plane.geometry.vertices[index].z);
-//             plane.add(skillMesh)
-//         });
-//     }
-// }
 
 // Sphere vertices alignement
 function alignTextOnSphereVertices() {
@@ -263,6 +205,8 @@ function alignTextOnSphereVertices() {
 
 // Sphere vertices alignement
 function alignText() {
+    // On réinitialise la position de la caméra
+    controls.reset()
     // On réinitialise la rotation de la sphere
     sphere.rotation.y = 0;
     sphere.rotation.x = 0;
@@ -282,7 +226,6 @@ function alignText() {
 
 // Change Text Position onclick
 function changeGeometry() {
-
     // On stop ou relance la rotation de la sphere
     if (sphereIsAnimate) {
         alignText();
@@ -310,9 +253,6 @@ var animate = function (time) {
 
     // Sphere Animation
     if (sphereIsAnimate) {
-        console.log(sphere.rotation.y)
-        console.log(sphere.rotation.x)
-
         sphere.rotation.y += 0.01;
         sphere.rotation.x += 0.01;
     }
@@ -322,20 +262,28 @@ var animate = function (time) {
     if (sphere) {
         for (let index = 0; index < sphere.children.length; index++) {
             sphere.children[index].lookAt(camera.position)
-            // console.log(sphere.children[0].position)
         }
     }
-
 
     renderer.render(scene, camera);
 }
 
+//----------------------------------------------------------------- 
+// RESIZE
 
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
 
 //----------------------------------------------------------------- 
 // LISTENER
-renderer.domElement.addEventListener("click", changeGeometry, true);
-
+window.addEventListener('click', changeGeometry, false);
+window.addEventListener('resize', onWindowResize, false);
 //----------------------------------------------------------------- 
 // START FUNCTIONS
 
